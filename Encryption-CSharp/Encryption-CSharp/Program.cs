6624da24d3e230e4b42deb6a3d9d1bbc470bda1a
druyang <andrw.k.yang.21@dartmouth.edu>
2020-05-26 16:39:55 -0500

    loads CSV as a 2D array of strings

--- Encryption-CSharp/Encryption-CSharp/Program.cs
+++ Encryption-CSharp/Encryption-CSharp/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.IO.Enumeration;
 
 namespace Encryption_CSharp
 {
-    class Program
+    class Encryption_Module
     {
-        static void Main(string[] args)
+        
+        private static string[,] CSV_Loader(string file)
         {
-            Console.WriteLine("Hello World!");
+            string all_file_data = System.IO.File.ReadAllText(file);
+
+            all_file_data = all_file_data.Replace('\n', '\r');
+            string[] rows = all_file_data.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int num_rows = rows.Length;
+            int num_cols = rows[0].Split(',').Length;
+
+            string[,] processed_data = new string[num_rows, num_cols]; 
+
+            for (int i = 0; i < num_rows; i++)
+            {
+                string[] a_row = rows[i].Split(',');
+                for (int j = 0; j < num_cols; j++)
+                {
+                    processed_data[i, j] = a_row[j]; 
+                }
+            }
+
+            return processed_data; 
+        }
+
+        static int Main(string[] args)
+        {
+
+            // Validate inputs 
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: encrypt.exe [INPUT CSV DATA] [OUTPUT CSV] [OUTPUT PRIVATE KEY] [OUTPUT PUBLIC KEY]");
+                return 1; 
+            }
+
+            // Load file locations 
+            String input_csv = args[0];
+            String output_csv = args[1];
+            String output_private_key = args[2];
+            String output_public_key = args[3];
+
+            // Load CSV into 2D Array 
+            string [,] data = CSV_Loader(input_csv);
+
+            // Encrypt CSV Array 
+
+            // Write CSV Array 
+
+            return 0; 
         }
     }
 }
