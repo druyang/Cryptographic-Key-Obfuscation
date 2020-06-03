42df7b0060fd0edf60654a3c16b9b3b4f71776fa
druyang <andrw.k.yang.21@dartmouth.edu>
2020-06-03 18:04:05 -0500

    Encryption fully implemented

--- Encryption-CSharp/Encryption-CSharp/Program.cs
+++ Encryption-CSharp/Encryption-CSharp/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualBasic.FileIO;
+using Microsoft.VisualBasic.FileIO;
 using System;
 using System.ComponentModel.Design;
 using System.Diagnostics;
@@ -14,11 +14,18 @@ namespace Encryption_CSharp
 {
     class Encryption_Module
     {
-
+        // Data Constants 
         public static int id_col = 0, 
                           gender_col = 2, 
                           age_col = 3 , 
-                          state_col = 17; 
+                          state_col = 17;
+
+        // Encryption Constants 
+        const UInt64 e = 963443092119039113;
+        const UInt64 d = 920403722748280569;
+        const UInt64 n = 2108958572404460311;
+
+
         public struct preProccessedData
         {
             public int num_rows;
@@ -58,6 +65,50 @@ namespace Encryption_CSharp
             return loaded_data; 
         }
 
+
+        // ENCRYPTION FUNCTIONS: 
+        // Computes (a * b) % n
+        private UInt64 modmult(UInt64 a, UInt64 b, UInt64 n)
+        {
+            UInt64 res = 0;
+            a = a % n; 
+             while (b > 0)
+            {
+                if (b % 2 == 1)
+                    res = (res + a) % n;
+
+                b = b >> 1;
+                a = (a * 2) % n; 
+            }
+
+            return res; 
+
+        }
+
+        // Computes (msg ** exponent) % n
+        private UInt64 modexp(UInt64 msg, UInt64 exponent, UInt64 n)
+        {
+            UInt64 res = 1; 
+            msg = msg % n; // Update msg if it is more than or equal to n
+            while (exponent > 0)
+            {
+                // If exponent is odd, multiply x with result
+                if (exponent % 2 == 1)
+                    res = modmult(res, msg, n);
+
+                // exponent must be even now
+                exponent = exponent >> 1; // exponent = exponent/2
+                msg = modmult(msg, msg, n); // compute (msg^2) % n
+            }
+
+            return res;
+        }
+
+        // Actual encryption function 
+        public UInt64 encryptCell(UInt64 data)
+        {
+            return modexp(data, e, n);
+        }
 
 
         private static UInt64[,] process_korea_format(preProccessedData raw_CSV)
@@ -134,7 +185,7 @@ namespace Encryption_CSharp
             return processed_data; 
 
         }
-        const string formatter = "{0,5}{1,17}{2,15}";
+        const string formatter = "{0,5}{1,27}{2,24}";
 
         // Convert four byte array elements to a uint and display it.
         public static void ProcessBA(byte[] bytes, int index)
@@ -155,7 +206,8 @@ namespace Encryption_CSharp
 
         static int Main(string[] args)
         {
-            bool verbose = true;
+            Encryption_Module helpers = new Encryption_Module(); 
+            bool verbose = false;
             int num_cols = 8;
             const int pad_bytes = 2; 
 
@@ -181,8 +233,8 @@ namespace Encryption_CSharp
             BinaryWriter file_writer = new BinaryWriter(File.Open(args[1], FileMode.Create));
 
             // Loop over rows
-            //for (int i = 0; i <= processed_data.GetUpperBound(0); i++)
-            for (int i = 0; i <= 1; i++)
+            for (int i = 0; i <= processed_data.GetUpperBound(0); i++)
+                //for (int i = 0; i <= 1; i++)
             {
                 if (processed_data[i,0] != 0)
                 {
@@ -203,10 +255,9 @@ namespace Encryption_CSharp
 
                     }
 
-                    // Randomize padded byte data 
-
-                        for (int k = 0; k < num_cols; k++)
-                        {
+                    // For each cell in the row  
+                    for (int k = 0; k < num_cols; k++)
+                    {
                         if (verbose)
                         {
                             //Console.WriteLine("Before Rand:");
@@ -214,16 +265,32 @@ namespace Encryption_CSharp
 
                         }
 
+                        // Pad 2 most significant bytes with random values 
                         RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
                         rng.GetBytes(byted_row, sizeof(UInt64) - pad_bytes + k * sizeof(UInt64), pad_bytes); 
                   
-                            if (verbose)
-                            {
-                                //Console.WriteLine("After Rand:");
-                                ProcessBA(byted_row, k * sizeof(UInt64));
+                        if (verbose)
+                        {
+                            //Console.WriteLine("After Rand:");
+                            ProcessBA(byted_row, k * sizeof(UInt64));
 
-                            }
-                        // Encrypt Bytes: 
+                        }
+
+                    }
+
+                    //Encrypt Bytes: 
+                    UInt64[] encrypted_values = new UInt64[num_cols];
+                    for (int current_cell = 0; current_cell < num_cols; current_cell++)
+                    {
+                        UInt64 c_value_int = BitConverter.ToUInt64(byted_row, sizeof(UInt64) * current_cell);
+                        encrypted_values[current_cell] = helpers.encryptCell(c_value_int);
+                        Buffer.BlockCopy(BitConverter.GetBytes(encrypted_values[current_cell]), 0, byted_row, current_cell * sizeof(UInt64), sizeof(UInt64));
+                        if (verbose)
+                        {
+                            //Console.WriteLine("After Rand:");
+                            ProcessBA(byted_row, current_cell * sizeof(UInt64));
+
+                        }
 
                     }
 
